683cf4e4fcba428f437d6d04aedab1137c5ce1ce
RNMancini <rebeccamancini.student@careerdevs.com>
2018-08-26 22:23:46 +0000

    JS update

--- script.js
+++ script.js
@@ -1,30 +1,53 @@
-$(document).ready(function() {
-  function weather() {
-    navigator.geolocation.getCurrentPosition(success, error);
-    
-    function success(pos) {
-      var lat = pos.coords.latitude;
-      var long = pos.coords.longitude;
-    }
-    function error() { console.log('error');}
-    function weather(lat, long) {
-    var URL = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}';
-    $.getJSON(URL, function(data){ console.log(data);
- updateDOM();     
+const loc = document.getElementById("location");
+const temNum = document.getElementById("temperature-num");
+const temScale = document.getElementById("weather-condition");
+const weatherCondit =document.getElementById("weather-icon");
+
+$function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+      getWeather (position.coords.latitude, position.coords.longitude);
+      });
+      } else {
+        loc.innerHTML = "Geolocation is having an error.";
+        }
+        }
+        
+    //get weather api
+    function getWeath(lat, long) {
+    get const root = "https://fcc-weather-api.glitch.me/api/current";
+fetch('${root}lat=${lat}&{long}', { method: "get" })    
+.then(resp => resp.json())
+.then(data => {
+  updateDataToUI(data.namae, data.weather, data.main.temp);
+  .catch(function(err) {
+    console.error(err);
     });
-  }
-  
-  
-  function updateDOM() {
-  
-    var city = data.name;
-   var temp = Math.round(data.main.temp); 
-   var desc = data.weather[0].description;
-    $('#city').html(city);
-  $('#temp').html(temp);
-    $('#desc').html(desc);
-    $('#icon').attr('src', icon);
+    }
     
+  
+  function updateDOM(location, weather, temp) {
+weatherIcon.innerHTML = '<img src="${weather[0].icon}"/>';
+weatherCon.innerHTML = weather[0].main;
+loc.innerHTML = location;
+tempNum.inner.html = '${temp}';
+}
+
+window.onload = function() {
+  getLocation();
+};
+
+function cToF(celsius) {
+  return celsius * 9/5 + 32;
+}
+  function fToC(fahrenheit)
+  { return (fahrenheit - 32) * 5/9;
+  }
+  function toggleScale() {
+    if (tempScale.innerHTML === "C") {
+     tempNum.innerHTML = cToF(tempNum.innerHTML).toFixed(2); 
+tempScale.innerHTML = "C";    }
   }
   
-});
+tempScale.addEventListener("click", toggleScale);
+
